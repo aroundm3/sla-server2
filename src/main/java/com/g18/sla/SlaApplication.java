4d87a171f57313642efd5883acd4c483ccf4c937
Nguyễn Thái Dương <loswkpq17@gmail.com>
2021-06-14 21:11:33 +0700

    init project

---
+++ src/main/java/com/g18/sla/SlaApplication.java
@@ -0,0 +1,13 @@
+package com.g18.sla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlaApplication.class, args);
+	}
+
+}
